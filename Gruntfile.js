f96c597252774a46848013438f02e4c48512d6a4
Devin Matte <devinmatte@gmail.com>
2016-12-08 16:29:05 -0500

    Set up app with actual bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,156 +1,126 @@
-/*
-* Gruntfile for bootstrap-csh
-*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-// Project config
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  // Minify CSS
-  cssmin: {
-    members: {
-      options: {
-        banner: '/*! <%= pkg.name %>/members.min.css, v<%= pkg.version %>, minified <%= grunt.template.today("yyyy-mm-dd") %> */'
-      },
-      files: [{
-        expand: true,
-        cwd: 'dev/',
-        src: ['members.css'],
-        dest: 'release/',
-        ext: '.min.css'
-      }]
-    },
-    membersflat: {
-      options: {
-        banner: '/*! <%= pkg.name %>/members-flat.min.css, v<%= pkg.version %>, minified <%= grunt.template.today("yyyy-mm-dd") %> */'
-      },
-      files: [{
-        expand: true,
-        cwd: 'dev/',
-        src: ['members-flat.css'],
-        dest: 'release/',
-        ext: '.min.css'
-      }]
-    },
-    public: {
-      options: {
-        banner: '/*! <%= pkg.name %>/public.min.css, v<%= pkg.version %>, minified <%= grunt.template.today("yyyy-mm-dd") %> */'
-      },
-      files: [{
-        expand: true,
-        cwd: 'dev/',
-        src: ['public.css'],
-        dest: 'release/',
-        ext: '.min.css'
-      }]
-    }
-  },
-  // Compile LESS to CSS
-  less: {
-    members: {
-      options: {
-        paths: ['dev/']
-      },
-      files: {
-        'dev/members.css': 'dev/members.less'
-      }
-    },
-    membersflat: {
-      options: {
-        paths: ['dev/']
-      },
-      files: {
-        'dev/members-flat.css': 'dev/members-flat.less'
-      }
-    },
-    public: {
-      options: {
-        paths: ['dev/']
-      },
-      files: {
-        'dev/public.css': 'dev/public.less'
-      }
-    }
-  },
-  // Watch for changes
-  watch: {
-    members: {
-      files: [ 'dev/members.less' ],
-      tasks: [ 'less:members', 'cssmin:members' ]
-    },
-     membersflat: {
-      files: [ 'dev/members-flat.less' ],
-      tasks: [ 'less:membersflat', 'cssmin:membersflat' ]
-    },
-    public: {
-      files: [ 'dev/public.less' ],
-      tasks: [ 'less:public', 'cssmin:public' ]
-    }
-  },
-  // Run a local server
-  connect: {
-    default: {
-      options: {
-        port: 9000,
-        keepalive: true,
-        base: './',
-        hostname: '*'
-      }
-    },
-    members: {
-      options: {
-        port: 9000,
-        keepalive: true,
-        base: './',
-        hostname: '*',
-        open: {
-          target: 'http://localhost:9000/test/members'
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buildDir: 'dist',
+        outputFile: '<%= buildDir %>/css/csh-material-bootstrap.css',
+        banner: '/*!\n' +
+        ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+        ' * Homepage: <%= pkg.homepage %>\n' +
+        ' * Copyright 2012-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        ' * Licensed under <%= pkg.license %>\n' +
+        ' * Based on Bootstrap\n' +
+        '*/\n',
+        clean: {
+            all: {
+                src: ['sass/build.scss', '<%= outputFile %>*', '<%= buildDir %>/fonts/*']
+            },
+            build: {
+                src: ['sass/build.scss']
+            }
+        },
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: false
+            },
+            dist: {
+                src: 'sass/csh-material-bootstrap.scss',
+                dest: 'sass/build.scss'
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    '<%= outputFile %>': ['sass/build.scss']
+                },
+                options: {
+                    style: 'expanded',
+                    precision: 8,
+                    bundleExec: true,
+                    'unix-newlines': true
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('pixrem')(),
+                    require('autoprefixer')({
+                        browsers: [
+                            "Android 2.3",
+                            "Android >= 4",
+                            "Chrome >= 20",
+                            "Firefox >= 24",
+                            "Explorer >= 8",
+                            "iOS >= 6",
+                            "Opera >= 12",
+                            "Safari >= 6"
+                        ]
+                    }),
+                    require('css-mqpacker')(),
+                    require('cssnano')({
+                        autoprefixer: false,
+                        safe: true,
+                        sourcemap: false
+                    })
+                ]
+            },
+            dist: {
+                src: '<%= outputFile %>'
+            }
+        },
+        copy: {
+            fonts: {
+                expand: true,
+                cwd: 'bower_components/bootstrap-sass/assets',
+                src: 'fonts/**',
+                dest: '<%= buildDir %>'
+            }
+        },
+        watch: {
+            files: ['sass/csh-material-bootstrap.scss', 'sass/variables.scss', 'index.html'],
+            tasks: 'build',
+            options: {
+                livereload: true,
+                nospawn: true
+            }
+        },
+        connect: {
+            base: {
+                options: {
+                    port: 3000,
+                    livereload: true,
+                    open: true
+                }
+            },
+            keepalive: {
+                options: {
+                    port: 3000,
+                    livereload: true,
+                    keepalive: true,
+                    open: true
+                }
+            }
         }
-      }
-    },
-    membersflat: {
-      options: {
-        port: 9000,
-        keepalive: true,
-        base: './',
-        hostname: '*',
-        open: {
-          target: 'http://localhost:9000/test/members-flat'
-        }
-      }
-    },
-    public: {
-      options: {
-        port: 9000,
-        keepalive: true,
-        base: './',
-        hostname: '*',
-        open: {
-          target: 'http://localhost:9000/test/public'
-        }
-      }
-    }
-  }
-});
+    });
 
-// Load plugins
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('build', ['clean:all', 'concat', 'sass:dist', 'postcss', 'clean:build', 'copy:fonts']);
 
-// Register tasks
-grunt.registerTask('default', ['less', 'cssmin']);
-grunt.registerTask('defaultMembers', ['less:members', 'cssmin:members']);
-grunt.registerTask('defaultMembersFlat', ['less:membersflat', 'cssmin:membersflat']);
-grunt.registerTask('defaultPublic', ['less:public', 'cssmin:public']);
-grunt.registerTask('dev', ['default', 'watch']);
-grunt.registerTask('devMembers', ['defaultMembers', 'watch:members']);
-grunt.registerTask('devMembersFlat', ['defaultMembersFlat', 'watch:membersflat']);
-grunt.registerTask('devPublic', ['defaultPublic', 'watch:public']);
-grunt.registerTask('test', ['connect:default']);
-grunt.registerTask('testMembers', ['connect:members']);
-grunt.registerTask('testMembersFlat', ['connect:membersflat']);
-grunt.registerTask('testPublic', ['connect:public']);
+    grunt.event.on('watch', function (action) {
+        var path = require('path');
+    });
 
+    grunt.registerTask('server', 'connect:keepalive');
+
+    grunt.registerTask('default', ['connect:base', 'watch']);
 };
